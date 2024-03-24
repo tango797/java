d9b9c11ef5f651b5aa0115cfa05f5774534f8e3c
Tango797 <tewatia35@gmail.com>
2024-03-24 11:28:48 +0530

    first

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static void main(String[] args) {
     System.out.println("Om namah shivay");
         System.out.println(5+0);
+
+
+
+        System.out.println("hhhe");
     }
 }
